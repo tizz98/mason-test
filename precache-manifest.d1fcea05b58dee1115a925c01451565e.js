3c1398de865769f42255c00b47f9f49a4ac040f4
Elijah Wilson <dev.tizz98@gmail.com>
2019-04-09 17:59:41 -0700

    Updates

---
+++ precache-manifest.d1fcea05b58dee1115a925c01451565e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "465800001ba282c10f4f",
+    "url": "/mason-test/static/js/runtime~main.5841ab50.js"
+  },
+  {
+    "revision": "6d8a70f298086de6c66b",
+    "url": "/mason-test/static/js/main.f7f0bf69.chunk.js"
+  },
+  {
+    "revision": "a43ba901ed787a5decd5",
+    "url": "/mason-test/static/js/2.6ea078e1.chunk.js"
+  },
+  {
+    "revision": "6d8a70f298086de6c66b",
+    "url": "/mason-test/static/css/main.46bf90ae.chunk.css"
+  },
+  {
+    "revision": "d06fb5f94c1d7f4af6fbf2f837ab58b4",
+    "url": "/mason-test/index.html"
+  }
+];
