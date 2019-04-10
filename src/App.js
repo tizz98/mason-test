d471809551c41edbc3d93f1852f8f67cf3de82da
Elijah Wilson <dev.tizz98@gmail.com>
2019-04-09 17:59:12 -0700

    test app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Mason, { Canvas } from 'mason-library';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    Mason({
+      apiKey:'ZLr4EYj8yAWv8qjt0IBTN16zziBwTYiICwjZvSuLI18=',
+      projectId: '5cad0dcd475b830003630ed8',
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Canvas id="5cad29d7475b830003630fbf" />
       </div>
     );
   }
